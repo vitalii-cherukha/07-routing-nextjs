776abd2af16d28b0be39d29d9a87f3038a18fccc
vitalii <vitalii.cherukha@gmail.com>
2025-08-29 16:44:27 +0300

    fix-btn

--- components/NotePreview/NotePreview.tsx
+++ components/NotePreview/NotePreview.tsx
@@ -1,7 +1,10 @@
 import { Note } from '@/types/note';
 import css from './NotePreview.module.css';
 
+import { useRouter } from 'next/navigation';
+
 const NotePreview = ({ note }: { note: Note }) => {
+  const router = useRouter();
   return (
     <div className={css.container}>
       <div className={css.item}>
@@ -11,7 +14,7 @@ const NotePreview = ({ note }: { note: Note }) => {
         <p className={css.content}>{note.content}</p>
         <p className={css.date}>{note.createdAt}</p>
         <p className={css.tag}>{note.tag}</p>
-        <button type="button" className={css.backBtn}>
+        <button onClick={() => router.back()} className={css.backBtn}>
           Close
         </button>
       </div>
